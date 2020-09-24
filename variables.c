83789e1918595dc48950bb8b94b5e957abf0d5b0
Yash kolte <yash.kolte9@gmail.com>
2020-09-24 22:03:34 +0530

    C

---
+++ variables.c
@@ -0,0 +1,13 @@
+# include<stdio.h>  //Pre prossesor directive
+
+int main(){
+    int a= 3;
+    // int b= 7.7; // Not recommended because 8.5 is not an integer 
+    float b= 7.7;
+    char c= 'u';
+
+    printf("The value of a is %d", a);   // \n for new line charactor
+    printf("The value of b is %f", b);
+    printf("The value of c is %c", c);
+    return 0;
+} 
