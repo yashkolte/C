af66e4e234573b58112276eb1a21b5aa6256a9dd
Yash kolte <yash.kolte9@gmail.com>
2020-10-06 09:12:07 +0530

    c

--- 31-Quiz.c
+++ 31-Quiz.c
@@ -7,19 +7,18 @@ void GoodNight();
 int main()
 {
     GoodMorning();
-    GoodAfterNoon();
-    GoodNight();
-
     return 0;
 }
 
 void GoodMorning()
 {
     printf("Hello Yash GoodMorning\n");
+    GoodAfterNoon();
 }
 void GoodAfterNoon()
 {
     printf("Hello Yash GoodAfterNoon\n");
+    GoodNight();
 }
 void GoodNight()
 {
