ca479040f6dcf24b6ac19dd6ecf40b904843add9
Yash kolte <yash.kolte9@gmail.com>
2020-10-06 08:59:40 +0530

    c

--- 29-Game.c
+++ 29-Game.c
@@ -13,7 +13,7 @@ int main()
     {
         printf("Guess the the number between 1 to 100 :\n");
         scanf("%d", &guess);
-        if (guess > number) 
+        if (guess > number)
         {
             printf("Lower number please!\n");
         }
---
+++ 31-Quiz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void GoodMorning();
+void GoodAfterNoon();
+void GoodNight();
+
+int main()
+{
+    GoodMorning();
+    GoodAfterNoon();
+    GoodNight();
+
+    return 0;
+}
+
+void GoodMorning()
+{
+    printf("Hello Yash GoodMorning\n");
+}
+void GoodAfterNoon()
+{
+    printf("Hello Yash GoodAfterNoon\n");
+}
+void GoodNight()
+{
+    printf("Hello Yash GoodNight\n");
+}
